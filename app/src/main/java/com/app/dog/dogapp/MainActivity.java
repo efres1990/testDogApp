51386255702b32fd48bba91565b05e6dabca45ef
efres1990 <e.freires@alumnos.urjc.es>
2017-07-15 20:59:20 +0200

    primer commit

--- app/src/main/java/com/app/dog/dogapp/MainActivity.java
+++ app/src/main/java/com/app/dog/dogapp/MainActivity.java
@@ -3,6 +3,10 @@ package com.app.dog.dogapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+
+/**
+ * @author eladiosuarez
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
